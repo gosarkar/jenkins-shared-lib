406bcddd090d9cfe2f1d49db354aaaa1cd8d28fe
gosarkar <gosarkar@in.ibm.com>
2019-02-17 13:15:37 +0530

    Update test1.groovy

--- src/rbs/group1/test1.groovy
+++ src/rbs/group1/test1.groovy
@@ -5,3 +5,10 @@ def sayHello(name) {
   println 'Hello '+name
   
 }
+
+def sonarAnalysis(sonarScannerHome){
+    echo 'Sonar analysis ..'
+    echo "build no = ${BUILD_NUMBER}"  
+  echo "sonarscanner home = ${sonarScannerHome}"
+  //bat sonarScannerHome+'/bin/sonar-scanner -Dsonar.projectname=ELibrary-pipeline -Dsonar.projectKey=ELibrary-pipeline -Dsonar.projectVersion=${BUILD_NUMBER} -Dsonar.sources=src -Dsonar.language=java -Dsonar.java.binaries=target'
+}
