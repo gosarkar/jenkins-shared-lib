e77707feac774b8f46ed52b220cbd207461e81d1
gosarkar <gosarkar@in.ibm.com>
2019-02-17 12:58:06 +0530

    Update test1.groovy

--- src/rbs/group1/test1.groovy
+++ src/rbs/group1/test1.groovy
@@ -1,5 +1,7 @@
 package rbs.group1;
 
 def sayHello(name) {
+  checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/gosarkar/pipelinescript.git']]]
   println 'Hello '+name
+  
 }
