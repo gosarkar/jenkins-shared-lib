67879cb8bc68bb464673ea73c55a94f563386133
gosarkar <gosarkar@in.ibm.com>
2019-04-19 18:28:38 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -12,7 +12,30 @@ ln = System.getProperty('line.separator')
 @groovy.transform.Field
 file = new File("C:/temp/log.txt")
 
+
+def file = new File('c:/temp/logoutput.txt')
+def dateFormatter = { line -> "${new Date()}: $line"}
+def debugFilter = { line -> line.contains('debug')}
+def genericFilter = { line -> true }
+def consoleAppender = {line -> println line}
+def fileAppender = {line -> file << "$line\n"}
+def configurator = { format, filter, line -> 
+  println 'inside configurator'
+  filter(line) ? format(line) :  null
+}
+def appender = {config, append, line -> 
+    println 'inside appender'
+  record = config(line)
+  if (record) append(record)
+}
+
+def config = configurator.curry(dateFormatter, genericFilter)
+def log = appender.curry(config, consoleAppender)
+
+
 def logMessage(message){
   echo "Log message: $message"
+  log('==>>>> logging....')
   file << "$message$ln"
 }
+
