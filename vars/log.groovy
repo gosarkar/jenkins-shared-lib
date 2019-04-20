f287723df4a0b620c9e0ed9a5aa24d9f85cde687
Goutam Sarkar <goutamsarkar@gmail.com>
2019-04-20 10:10:40 +0530

    updated

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,34 +1,46 @@
 @groovy.transform.Field
-INFO=1
-
-@groovy.transform.Field
-WARN=2
-
-ERROR=3
-
-@groovy.transform.Field
 ln = System.getProperty('line.separator')
+level = logLevel.ALL
+generalLogFile = new File("c:/temp/log.txt")
+errorLogFile = new File("c:/temp/error.txt")
 
-@groovy.transform.Field
-file = new File("C:/temp/log.txt")
-
-
-def file = new File('c:/temp/logoutput.txt')
-def dateFormatter = { line -> "${new Date()}: $line"}
-def debugFilter = { line -> line.contains('debug')}
-def genericFilter = { line -> true }
-def consoleAppender = {line -> println line}
-def fileAppender = {line -> file << "$line\n"}
-def log = { format, filter, append, line -> 
-  println 'inside configurator'
-  record = filter(line) ? format(line) :  null
-  if(record) append(line)
+def setLevel(level){
+    echo "received level = $level"
+    this.level = level 
 }
 
-
-def logMessage(message){
-  echo "Log message: $message"
-  log(dateFormatter, genericFilter, consoleAppender, "==>>logging: $message")
-  file << "$message$ln"
+def canBeLogged(level){
+    echo "received level = $level"
+    echo "level set = $this.level"
+    this.level <= level ? true : false 
 }
 
+def logMessage(message, level){
+    if(!canBeLogged(level)){
+        echo "Cannot be logged"
+        return
+    } 
+    formattedMessage = getFormattedMessage(formattedMessage)
+    echo "$formattedMessage"
+    generalLogFile << "$message$ln"
+}
+
+def debug(message){
+    logMessage(message, logLevel.DEBUG)
+}
+
+def info(message){
+    logMessage(message, logLevel.INFO) 
+}
+
+def warn(message){
+    logMessage(message, logLevel.WARN) 
+}
+
+def error(message){
+    logMessage(message, logLevel.ERROR) 
+}
+
+def getFormattedMessage(message){
+    return "${new Date()} - $JOB_NAME - $BUILD_NUMBER : $message" 
+}
---
+++ vars/logLevel.groovy
@@ -0,0 +1,23 @@
+/* All levels */
+@groovy.transform.Field
+ALL=0
+
+/* Designates debug messages  */
+@groovy.transform.Field
+DEBUG=1
+
+/* Designates informational messages */
+@groovy.transform.Field
+INFO=2
+
+/* Designates potentially harmful situations */
+@groovy.transform.Field
+WARN=3
+
+/* Designates error events which stops the execution */
+@groovy.transform.Field
+ERROR=4
+
+/* The highest possible level intended to turn off logging */
+@groovy.transform.Field
+OFF=5
