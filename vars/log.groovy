b178ebd9a2e16ce8a2a4fef403de6ccc4a097699
Goutam Sarkar <goutamsarkar@gmail.com>
2019-04-20 10:47:07 +0530

    updated

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,7 +1,7 @@
-@groovy.transform.Field
-ln = System.getProperty('line.separator')
+//@groovy.transform.Field
+//ln = System.getProperty('line.separator')
 
-@groovy.transform.Field
+//@groovy.transform.Field
 //level = logLevel.ALL
 
 //@groovy.transform.Field
@@ -10,17 +10,20 @@ ln = System.getProperty('line.separator')
 //@groovy.transform.Field
 //errorLogFile = new File("c:/temp/error.txt")
 
-def setLevel(level){
-    echo "received level = $level"
-    this.level = level 
-}
+//def setLevel(level){
+//    echo "received level = $level"
+//    this.level = level 
+//}
 
+/*
 def canBeLogged(level){
     echo "received level = $level"
     echo "level set = $this.level"
     this.level <= level ? true : false 
 }
+*/
 
+/*
 def logMessage(message, level){
     if(!canBeLogged(level)){
         echo "Cannot be logged"
@@ -30,7 +33,9 @@ def logMessage(message, level){
     echo "$formattedMessage"
     //generalLogFile << "$message$ln"
 }
+*/
 
+/*
 def debug(message){
     echo "debug: $message"
     //logMessage(message, logLevel.DEBUG)
@@ -56,6 +61,7 @@ def getFormattedMessage(message, level){
     //return "${new Date()} - $JOB_NAME - $BUILD_NUMBER $ln${logLevel.getLevelName(level)}: $message$ln" 
     return "message"
 }
+*/
 
 def test(message){
     echo "testing: $message"
