51dc3047e527e66793bf170e6bc62e140e4e2ccc
Goutam Sarkar <goutamsarkar@gmail.com>
2019-04-20 12:04:56 +0530

    updated

--- vars/log.groovy
+++ vars/log.groovy
@@ -12,13 +12,13 @@ generalLogFile = new File("c:/temp/log.txt")
 @groovy.transform.Field
 errorLogFile = new File("c:/temp/error.txt")
 
+@groovy.transform.Field
 dateTimeFormat = "MMMM dd yyyy HH:mm:ss a"
 
 def setLevel(requestedLevel){
     echo "received level = $requestedLevel"
     level = requestedLevel 
 }
-
 
 def canBeLogged(requestedLevel){
     echo "received level = $requestedLevel"
@@ -32,6 +32,10 @@ def getCurrentDateTime(){
     return sdf.format(currentDate)
 }
 
+def isErrorMessage(requestedLevel){
+    return logLevel.ERROR == requestedLevel
+}
+
 def logMessage(message, requestedLevel){
     if(!canBeLogged(requestedLevel)){
         echo "Cannot be logged"
@@ -39,7 +43,10 @@ def logMessage(message, requestedLevel){
     } 
     formattedMessage = getFormattedMessage(message, requestedLevel)
     echo "$formattedMessage"
-    generalLogFile << "$formattedMessage$ln"
+    generalLogFile << "$formattedMessage $ln"
+    if(isErrorMessage(requestedLevel)){
+        errorLogFile << "$formattedMessage $ln"
+    }
 }
 
 
@@ -66,7 +73,7 @@ def error(message){
 
 def getFormattedMessage(message, level){
     echo "getFormattedMessage: $message $level"
-    return "${getCurrentDateTime()} - $JOB_NAME - Build No - $BUILD_NUMBER $ln${logLevel.getLevelName(level)}: $message$ln" 
+    return "${getCurrentDateTime()} - $JOB_NAME - Build No - $BUILD_NUMBER $ln${logLevel.getLevelName(level)}: $message" 
 }
 
 
@@ -79,3 +86,4 @@ def test(message){
     //echo "debug level name ${logLevel.getLevelName(logLevel.DEBUG)}"
     echo "${new Date()} - $JOB_NAME - $BUILD_NUMBER $ln${logLevel.getLevelName(level)}: $message$ln" 
 }
+
